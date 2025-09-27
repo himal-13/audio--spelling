41e0b88190bcd8b0ae7e3760cc5d64e09794d531
himal-13 <mememimi413@gmail.com>
2025-09-27 12:32:10 +0545

    build change

--- audio_spelling/android/app/build.gradle.kts
+++ audio_spelling/android/app/build.gradle.kts
@@ -1,3 +1,7 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -5,10 +9,25 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
 android {
-    namespace = "com.example.audio_spelling"
+    namespace = "com.wordgame.wordora"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
+
+        signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -21,10 +40,10 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.audio_spelling"
+        applicationId = "com.wordgame.wordora"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -34,7 +53,7 @@ android {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
--- audio_spelling/android/app/src/main/kotlin/com/wordgame/wordora/MainActivity.kt
+++ audio_spelling/android/app/src/main/kotlin/com/wordgame/wordora/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.audio_spelling
+package com.wordgame.wordora
 
 import io.flutter.embedding.android.FlutterActivity
 
